7b1cf97cb0922f4cdc5cd9962078e3e1c0cb1bee
itsyagirlV <154456371+itsyagirlLavender007@users.noreply.github.com>
2024-08-23 19:05:30 +0530

    04-article-views-I.sql

--- 04-article-views-I.sql
+++ 04-article-views-I.sql
@@ -22,10 +22,4 @@ Return the result table sorted by id in ascending order.
 
 -- Write your MySQL query statement below
 
-SELECT 
-DISTINCT 
-author_id 
-AS id 
-FROM Views 
-WHERE author_id = viewer_id 
-ORDER BY id 
+SELECT DISTINCT author_id AS id FROM Views WHERE author_id = viewer_id ORDER BY id 
