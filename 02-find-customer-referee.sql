73b36ad3e68035fef0f4e0d0c3d630981cbed0b4
itsyagirlV <154456371+itsyagirlLavender007@users.noreply.github.com>
2024-08-23 18:53:38 +0530

    02-find-customer-referee.sql

--- 02-find-customer-referee.sql
+++ 02-find-customer-referee.sql
@@ -18,7 +18,7 @@ Return the result table in any order. */
 
 -- Write your MySQL query statement below
 
-SELECT name from Customer WHERE referee_id is NULL OR referee_id != 2 
+SELECT name FROM Customer WHERE referee_id is NULL OR referee_id != 2 
 
 
 /*
