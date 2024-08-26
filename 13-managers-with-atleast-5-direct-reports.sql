43ddeeaddae14fc5f54c6e311729bf06eb7015e5
itsyagirlV <154456371+itsyagirlLavender007@users.noreply.github.com>
2024-08-26 22:44:45 +0530

    Add files via upload

---
+++ 13-managers-with-atleast-5-direct-reports.sql
@@ -0,0 +1,32 @@
+/*
+
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| department  | varchar |
+| managerId   | int     |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the name of an employee, their department, and the id of their manager.
+If managerId is null, then the employee does not have a manager.
+No employee will be the manager of themself.
+ 
+
+Write a solution to find managers with at least five direct reports.
+
+Return the result table in any order.
+
+*/
+
+-- Write your MySQL query statement below
+
+SELECT emp.name
+FROM Employee emp
+INNER JOIN Employee man
+ON emp.id = man.managerId
+GROUP BY man.managerId
+HAVING COUNT(man.managerId) >= 5
