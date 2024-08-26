ab3bb1d1f002c6c8fa7ce6cce124bcfcce4b1d72
itsyagirlV <154456371+itsyagirlLavender007@users.noreply.github.com>
2024-08-26 23:02:09 +0530

    15-boring-movies.sql

--- 15-boring-movies.sql
+++ 15-boring-movies.sql
@@ -23,4 +23,4 @@ Return the result table ordered by rating in descending order.
 
 -- Write your MySQL query statement below
 
-SELECT * FROM Cinema WHERE id%2 != 0 AND description != 'boring' ORDER BY rating DESC
+SELECT * FROM Cinema WHERE id % 2 != 0 AND description != 'boring' ORDER BY rating DESC
