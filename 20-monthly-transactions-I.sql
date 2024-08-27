11658e575184714025d2779e415aae0c3edad43b
itsyagirlV <154456371+itsyagirlLavender007@users.noreply.github.com>
2024-08-27 20:01:00 +0530

    20-monthly-transactions-I.sql

--- 20-monthly-transactions-I.sql
+++ 20-monthly-transactions-I.sql
@@ -24,12 +24,12 @@ Return the result table in any order.
 
 -- Write your MySQL query statement below
 
--- SELECT 
--- DATE_FORMAT(trans_date , '%Y-%m') AS month, 
--- country,
--- COUNT(id) as trans_count,
--- SUM(state = 'approved') AS approved_count,
--- SUM(amount) AS trans_total_amount,
--- SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
--- FROM Transactions
--- GROUP BY month, country
+SELECT 
+DATE_FORMAT(trans_date , '%Y-%m') AS month, 
+country,
+COUNT(id) as trans_count,
+SUM(state = 'approved') AS approved_count,
+SUM(amount) AS trans_total_amount,
+SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
+FROM Transactions
+GROUP BY month, country
