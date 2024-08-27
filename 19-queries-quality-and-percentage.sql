8534e3d27c17a44f9bc183cb952ab62f787f1b89
itsyagirlV <154456371+itsyagirlLavender007@users.noreply.github.com>
2024-08-27 20:01:18 +0530

    19-queries-quality-and-percentage.sql

--- 19-queries-quality-and-percentage.sql
+++ 19-queries-quality-and-percentage.sql
@@ -34,7 +34,7 @@ Return the result table in any order.
 
 -- Write your MySQL query statement below
 
--- SELECT query_name, ROUND(AVG(rating/position),2) AS Quality, ROUND(AVG(IF (rating<3 ,1,0))*100,2) AS poor_query_percentage 
--- FROM Queries
--- WHERE query_name IS NOT NULL
--- GROUP BY query_name
+SELECT query_name, ROUND(AVG(rating/position),2) AS Quality, ROUND(AVG(IF (rating<3 ,1,0))*100,2) AS poor_query_percentage 
+FROM Queries
+WHERE query_name IS NOT NULL
+GROUP BY query_name
